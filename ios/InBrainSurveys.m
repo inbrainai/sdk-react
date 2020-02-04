309d88306605bf7380a2c6a59b8f6c84a86845f8
Paul Saulue <psaulue@gmail.com>
2020-02-04 16:15:34 +0000

    Skeleton for iOS bridge. Two methods implemented (init and showSurveys)

--- ios/InBrainSurveys.m
+++ ios/InBrainSurveys.m
@@ -5,10 +5,26 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+// ****************
+// ***** INIT *****
+// ****************
+RCT_EXPORT_METHOD(init:(NSString *)clientId clientSecret:(nonnull NSString *)clientSecret resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
     // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberrrrArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    resolve(@[[NSString stringWithFormat: @"REsolve promise clientId: %@ clientSecret: %@", clientId, clientSecret]]);
 }
+
+// ****************
+// ***** INIT *****
+// ****************
+RCT_EXPORT_METHOD(showSurveys:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+    // TODO: Implement some actually useful functionality
+    resolve(@[[NSString stringWithFormat: @"Resolve show clientId"]]);
+}
+
+// ****************
+// ***** INIT *****
+// ****************
 
 @end
