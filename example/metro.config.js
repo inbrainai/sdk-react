2883ae7d040355c410244687a66fb64b49ba8cbf
Andrey <musiikoas@gmail.com>
2024-06-03 16:25:42 +0300

    clean up

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,15 +1,3 @@
-// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-// /**
-//  * Metro configuration
-//  * https://facebook.github.io/metro/docs/configuration
-//  *
-//  * @type {import('metro-config').MetroConfig}
-//  */
-// const config = {};
-
-// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
 const path = require('path');
 const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
@@ -43,7 +31,6 @@ const config = {
 
     extraNodeModules: modules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
-      // acc['inbrain-surveys'] = {root: path.resolve(__dirname, '..')};
       acc['inbrain-surveys'] = path.resolve(__dirname, '../');
       return acc;
     }, {}),
@@ -51,16 +38,3 @@ const config = {
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
-    // extraNodeModules: {
-    //   // Указываем, где искать вашу библиотеку
-    //   'inbrain-surveys': path.resolve(__dirname, '../'), // Путь к корневой директории вашей библиотеки
-    //   // Указываем, что react и react-native должны браться из вашей библиотеки
-    //   // 'react': path.resolve(__dirname, '../node_modules/react'),
-    //   // 'react-native': path.resolve(__dirname, '../node_modules/react-native'),
-    //   // Добавьте другие модули, если они также используются
-    //   // '@': path.resolve(__dirname, '../node_modules'),
-    //   // '@react-navigation/native-stack': path.resolve(__dirname, './node_modules/@react-navigation/native-stack'),
-    //   // '@react-navigation/native': path.resolve(__dirname, './node_modules/@react-navigation/native'),
-    //   // '@react-navigation/core': path.resolve(__dirname, './node_modules/@react-navigation/core'),
-    // },
